e47fc6338178a3cc28c963383c098df0051ce556
Tristan Goncalves <75995875+tristan-goncalves@users.noreply.github.com>
2025-06-17 15:47:59 +0200

    Conversion finalisé

--- api/src/conversion/conversion.module.ts
+++ api/src/conversion/conversion.module.ts
@@ -8,8 +8,6 @@ import { ConversionEntity } from './entities/conversion.entity';
 @Module({
   imports: [
     TypeOrmModule.forFeature([ConversionEntity]),
-
-    // Client RabbitMQ pour la conversion
     ClientsModule.register([
       {
         name: 'CONVERSION_SERVICE',
@@ -17,17 +15,6 @@ import { ConversionEntity } from './entities/conversion.entity';
         options: {
           urls: ['amqp://rabbitmq:5672'],
           queue: 'convert-docx-to-pdf',
-          queueOptions: {
-            durable: true,
-          },
-        },
-      },
-      {
-        name: 'RMQ_UPLOAD_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://rabbitmq:5672'],
-          queue: 'upload-docx-request',
           queueOptions: {
             durable: true,
           },
